73e13fd04105c8efce6f38917fd7f5070b815cee
Highfredo <highfredo@gmail.com>
2015-01-22 23:41:18 +0100

    unido gulpfile de juan con el de jhipster.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,12 @@ var gulp = require('gulp'),
     flatten = require('gulp-flatten'),
     clean = require('gulp-clean'),
     replace = require('gulp-replace'),
-    url = require('url');
+    url = require('url'),
+    nib = require('nib'),
+    inject = require('gulp-inject'),
+    wiredep = require('wiredep').stream,
+    stylus= require('gulp-stylus'),
+    historyApiFallBack = require('connect-history-api-fallback');
 
 var karma = require('gulp-karma')({configFile: 'src/test/javascript/karma.conf.js'});
 
@@ -31,6 +36,25 @@ var yeoman = {
     apiPort: 8080,
     liveReloadPort: 35729
 };
+
+gulp.task('inject',function(){
+	var sources= gulp.src([yeoman.app+"scripts/**/*.js", yeoman.app+"assets/styles/**/*.css"]);
+	return gulp.src(yeoman.app+'index.html',{cwd: yeoman.app})
+	  .pipe(inject(sources,{
+	      read:false,
+	      ignorePath: yeoman.app
+	  }))
+	  .pipe(gulp.dest(yeoman.app));
+});
+
+gulp.task('wiredep',function(){
+    gulp.src(yeoman.app+'index.html')
+    .pipe(wiredep({
+        directory: yeoman.app + '/lib'
+    }))
+    .pipe(gulp.dest(yeoman.app));
+});
+
 
 gulp.task('clean', function() {
     return gulp.src(yeoman.dist, {read: false}).
@@ -61,8 +85,9 @@ gulp.task('images', function() {
         pipe(connect.reload());
 });
 
-gulp.task('styles', [], function() {
+gulp.task('css', [], function() {
     return gulp.src(yeoman.app + 'assets/styles/**/*.css').
+    	pipe(stylus({ use: nib() })).
         pipe(gulp.dest(yeoman.tmp)).
         pipe(connect.reload());
 });
@@ -87,10 +112,15 @@ gulp.task('server', ['watch'], function() {
 
     connect.server({
         root: [yeoman.app, yeoman.tmp],
+        // hostname: '0.0.0.0',
         port: yeoman.port,
         livereload: {
             port: yeoman.liveReloadPort
-        },
+        },/*
+        middleware: function(connect,opt) {
+            return [ historyApiFallBack ];
+        }*/
+        
         middleware: function() {
             // Build a list of proxies for routes: [route1_proxy, route2_proxy, ...]
             return proxyRoutes.map(function (r) {
@@ -99,9 +129,11 @@ gulp.task('server', ['watch'], function() {
                 return proxy(options);
             });
         }
+        
     });
 });
 
+// Aqui juan lanza jshint, pero no veo necesario hacerlo cada vez que se edita algo
 gulp.task('watch', function() {
     gulp.watch(yeoman.app + '*.html', ['usemin']);
     gulp.watch(yeoman.app + 'scripts/**', ['scripts']);
@@ -137,11 +169,12 @@ gulp.task('server:dist', ['build'], function() {
     });
 });
 
-gulp.task('build', ['copy'], function () {
+gulp.task('build', ['inject', 'copy'], function () {
     gulp.run('usemin');
 });
 
-gulp.task('usemin', ['images', 'styles'], function() {
+// juan lo llama "html", pero parece que esta tarea hace mas cosas
+gulp.task('usemin', ['images', 'css'], function() {
     return gulp.src(yeoman.app + '*.html').
         pipe(usemin({
             css: [
@@ -168,10 +201,9 @@ gulp.task('usemin', ['images', 'styles'], function() {
 gulp.task('jshint', function() {
     return gulp.src(['gulpfile.js', yeoman.app + 'scripts/**/*.js'])
         .pipe(jshint('.jshintrc'))
-        .pipe(jshint.reporter('jshint-stylish'));
+        .pipe(jshint.reporter('jshint-stylish'))
+        .pipe(jshint.reporter('fail'));
 });
 
-gulp.task('default', function() {
-    gulp.run('test');
-    gulp.run('build');
-});
+gulp.task('default',['server', 'inject', 'wiredep']);
+
